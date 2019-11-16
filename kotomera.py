f0ddaf1acc697793ef1b601f37390b77b9715526
Cosiek <fitek2@o2.pl>
2019-11-16 17:46:09 +0100

    Use picamera to make a picture on motion detection

--- kotomera.py
+++ kotomera.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
-from gpiozero import MotionSensor
+from datetime import datetime
 from signal import pause
+
+from gpiozero import MotionSensor
+from picamera import PiCamera
+
+
+CAMERA = PiCamera()
+CAMERA.resolution = (1024, 768)
 
 
 def it_is_on():
     print("It is on!")
+    name = str(datetime.now()).replace(" ", "_")
+    CAMERA.capture(f'{name}.jpg')
 
 
 def it_is_off():
