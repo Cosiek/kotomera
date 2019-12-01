834efce38adba12fbd971ea56f7a6a438df9b761
Cosiek <fitek2@o2.pl>
2019-12-01 02:06:04 +0100

    Few fixes for taking a picture

--- camera.py
+++ camera.py
@@ -36,6 +36,7 @@ def get_camera(**kwargs):
     opts = get_camera_options()
     camera = PiCamera(**opts)
     adjust_camera(camera, **kwargs)
+    return camera
 
 # CAMERA MODES ================================================================
 
